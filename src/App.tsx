44e8c421a6ea394059b79c08034f1f95bef790e1
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-09-12 14:51:56 -0400

    use provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
+import { Provider } from "react-redux";
 import { Navbar } from "./components";
 import { Home } from "./pages";
 import "./App.css";
 import { LayoutContainer } from "./styled-components";
+import store from "./redux/store";
 
 const App = () => {
   return (
-    <>
+    <Provider store={store}>
       <Navbar />
       <LayoutContainer>
         <Home />
       </LayoutContainer>
-    </>
+    </Provider>
   );
 };
 
