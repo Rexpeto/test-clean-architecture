6cfe928ba5a41dd05d1274dc4b284018233d8e91
Carlos Gallardo <carlosgallardoparra@hotmail.com>
2023-09-13 13:59:56 -0400

    new button open modal

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,34 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Button, Toolbar, Typography } from "@mui/material";
+import { CustomDialog, FavoriteTable } from "..";
+import { FavoriteBorder } from "@mui/icons-material";
+import { dialogOpenSubject$ } from "../CustomDialog/CustomDialog";
 
 const Navbar = () => {
+  const handleClick = () => {
+    dialogOpenSubject$.setSubject = true;
+  };
   return (
-    <AppBar position="fixed">
-      <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Technical Test
-        </Typography>
-      </Toolbar>
-    </AppBar>
+    <>
+      <CustomDialog>
+        <FavoriteTable />
+      </CustomDialog>
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Technical Test
+          </Typography>
+          <Button
+            variant="outlined"
+            color="inherit"
+            startIcon={<FavoriteBorder />}
+            size="small"
+            onClick={handleClick}
+          >
+            Open Favorites
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </>
   );
 };
 
